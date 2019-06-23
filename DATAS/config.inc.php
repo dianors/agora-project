e890150c7de8b72e4e24d8faf8b8d744913591eb
dianors <achkash@yahoo.fr>
2019-06-23 21:10:08 +0100

    commit all

--- DATAS/config.inc.php
+++ DATAS/config.inc.php
@@ -1,11 +1,11 @@
 <?php
 //CONNEXION À LA BDD
 define("db_host", "localhost");
-define("db_login", "");
-define("db_password", "");
-define("db_name", "");
+define("db_login", "root");
+define("db_password", "root");
+define("db_name", "agora");
 
 //ESPACE DISQUE / NB USERS / SALT
 define("limite_espace_disque", "10737418240");
 define("limite_nb_users", "10000");
-define("AGORA_SALT", "");
+define("AGORA_SALT", "b40411ca");
